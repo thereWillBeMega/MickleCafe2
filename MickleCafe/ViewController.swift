df167481313cd52a50dbfd310c0ca9e232b8b825
Peter Mickle <peter.mickle26@d155.org>
2024-09-16 08:46:06 -0500

    i frogor

--- MickleCafe/ViewController.swift
+++ MickleCafe/ViewController.swift
@@ -19,12 +19,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var cartOutlet: UILabel!
     
     
+    @IBOutlet weak var costOutlet: UILabel!
+    
     @IBOutlet weak var inputOutlet: UITextField!
     
     
     @IBOutlet weak var quantityOulet: UITextField!
     
-
+    var totalCost = 0.0
     
     var menu : [String] = ["Coffee", "Pancakes", "Toast", "Scrambled eggs"]
     
@@ -53,30 +55,47 @@ class ViewController: UIViewController {
 
     
     @IBAction func addAction(_ sender: UIButton) {
-        var quant = quantityOulet.text!
-        var quantity = Int(quant) ?? 0
+        print("added nothing!")
+        let quant = quantityOulet.text!
+        let quantity = Int(quant) ?? 0
         var curItem = ""
         var itemFound = false
+        var curCost = 0.0
         
+        
+        for i in 0..<menu.count{
+            if menu[i] == inputOutlet.text {
+                 curCost = prices[i]
+                curItem = menu[i]
+                itemFound = true
+            }
+        }
+
         for item in menu{
             if item == inputOutlet.text {
+                 
                 curItem = item
                 itemFound = true
             }
         }
             if !itemFound{
                 errorOutlet.text = "enter a valid menu item"
+            }else{
+                if quantity > 0{
+                    
+                    cartText += curItem + "\nQuantitiy : " + String(quantity) + "\n"
+                    cartOutlet.text = cartText
+                    totalCost += curCost * Double(quantity)
+                    costOutlet.text = "Cart Cost: " + String(totalCost)
+             print("added something")
+                }else{
+                    errorOutlet.text = "enter a positive number in quantity"
+                }
+            }
             }
 
         
-        if quantity > 0{
-            
-            cartText += curItem + "\nQuantitiy :" + String(quantity)
-            
-        }else{
-            errorOutlet.text = "enter a positive number in quantity"
-        }
-    }
+       
     
     
     @IBAction func adminAction(_ sender: Any) {
